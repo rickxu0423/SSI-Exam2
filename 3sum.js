f249b157fd0df4c88737dd420cf92e18c0da1354
Jiyun Xu <xjy2017cn@hotmail.com>
2019-12-30 13:59:46 -0800

    formatting

--- 3sum.js
+++ 3sum.js
@@ -15,10 +15,13 @@ var threeSum = function(nums) {
         while (left < right) {
             if (nums[left] + nums[right] == target) {
                 ans.push( [ nums[i], nums[left], nums[right] ] );
+                
                 left++;
                 right--;
+
                 while (left < right && nums[left] == nums[left- 1]) left ++;
                 while (left < right && nums[right] == nums[right + 1]) right --;
+
             } else if (nums[left] + nums[right] < target) {
                 left++;
             } else {
